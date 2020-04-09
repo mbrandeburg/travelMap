ebf9f4f05a6f7f5624c05dd082708910828e0e3f
Matthew Brandeburg <matthewbrandeburg@datamachines.io>
2020-04-09 17:01:29 -0400

    website nearly operational - need to fix iterator now that broken out

--- mapBuilder.py
+++ mapBuilder.py
@@ -11,67 +11,65 @@ def buildDF():
     initialDF['Have Been'][initialDF['Code'] == 'USA'] = 7 ## Infinity value
     initialDF['Year Went'][initialDF['Code'] == 'USA'] = 'Home'
 
-    ''' TODO: this will get replciated in another page we interact with for 
-    adding new trips. You'll use datetime for current year:
-    from datetime import datetime
-    datetime.now().year 
-    '''
     ## Add Base Trips 
-    initialDF = addTrip(initialDF,'Bahamas', 2006)
-    initialDF = addTrip(initialDF,'Mexico', 2007)
-    initialDF = addTrip(initialDF,'Jamaica', 2007)
-    initialDF = addTrip(initialDF,'Cayman Islands', 2007)
-    initialDF = addTrip(initialDF,'Italy', 2009)
-    initialDF = addTrip(initialDF,'Vatican', 2009)
-    initialDF = addTrip(initialDF,'Greece', 2009)
-    initialDF = addTrip(initialDF,'Haiti', 2009)
-    initialDF = addTrip(initialDF,'Costa Rica', 2010)
-    initialDF = addTrip(initialDF,'France', "Summer of 2010")
-    initialDF = addTrip(initialDF,'United Kingdom', 2010)
-    initialDF = addTrip(initialDF,'Germany', 2010)
-    initialDF = addTrip(initialDF,'Spain', "Study Abroad 2011-2012") ## NOTE: more than one must be a string, or else it tries to do math (i.e., returns -1 in map rather than 2011-2012)
-    initialDF = addTrip(initialDF,'France', 2011)
-    initialDF = addTrip(initialDF,'Ireland', 2011)
-    initialDF = addTrip(initialDF,'Portugal', 2011)
-    initialDF = addTrip(initialDF,'Andorra', 2011)
-    initialDF = addTrip(initialDF,'Netherlands', 2012)
-    initialDF = addTrip(initialDF,'Netherlands', 2012)
-    initialDF = addTrip(initialDF,'Mexico', 2013)
-    initialDF = addTrip(initialDF,'South Korea', "Worked 2013-2014")
-    initialDF = addTrip(initialDF,'Thailand', 2014)
-    initialDF = addTrip(initialDF,'Japan', 2014)
-    initialDF = addTrip(initialDF,'Japan', 2014) ## NOTE: Yep, 2x same year
-    initialDF = addTrip(initialDF,'Israel', 2015)
-    initialDF = addTrip(initialDF,'Palestine', 2015)
-    initialDF = addTrip(initialDF,'Germany', 2016)
-    initialDF = addTrip(initialDF,'Senegal', 2016)
-    initialDF = addTrip(initialDF,'Argentina', 2016)
-    initialDF = addTrip(initialDF,'Japan', 2017)
-    initialDF = addTrip(initialDF,'United Kingdom', 2017)
-    initialDF = addTrip(initialDF,'Portugal', 2017)
-    initialDF = addTrip(initialDF,'Spain', 2017)
-    initialDF = addTrip(initialDF,'South Africa', 2017)
-    initialDF = addTrip(initialDF,'China', 2018)
-    initialDF = addTrip(initialDF,'Germany', 2018)
-    initialDF = addTrip(initialDF,'Egypt', 2018)
-    initialDF = addTrip(initialDF,'Jordan', 2018)
-    initialDF = addTrip(initialDF,'Italy', 2019)
-    initialDF = addTrip(initialDF,'Vatican City', 2019)
-    initialDF = addTrip(initialDF,'France', 2019)
-    initialDF = addTrip(initialDF,'Vietnam', 2019)
-    initialDF = addTrip(initialDF,'Kenya', 2020)
+    initialDF.to_csv('initiallyPopulated.csv', index=False)
+    initialDF = addTrip('Bahamas', 2006)
+    initialDF = addTrip('Mexico', 2007)
+    initialDF = addTrip('Jamaica', 2007)
+    initialDF = addTrip('Cayman Islands', 2007)
+    initialDF = addTrip('Italy', 2009)
+    initialDF = addTrip('Vatican', 2009)
+    initialDF = addTrip('Greece', 2009)
+    initialDF = addTrip('Haiti', 2009)
+    initialDF = addTrip('Costa Rica', 2010)
+    initialDF = addTrip('France', "Summer of 2010")
+    initialDF = addTrip('United Kingdom', 2010)
+    initialDF = addTrip('Germany', 2010)
+    initialDF = addTrip('Spain', "Study Abroad 2011-2012") ## NOTE: more than one must be a string, or else it tries to do math (i.e., returns -1 in map rather than 2011-2012)
+    initialDF = addTrip('France', 2011)
+    initialDF = addTrip('Ireland', 2011)
+    initialDF = addTrip('Portugal', 2011)
+    initialDF = addTrip('Andorra', 2011)
+    initialDF = addTrip('Netherlands', 2012)
+    initialDF = addTrip('Netherlands', 2012)
+    initialDF = addTrip('Mexico', 2013)
+    initialDF = addTrip('South Korea', "Worked 2013-2014")
+    initialDF = addTrip('Thailand', 2014)
+    initialDF = addTrip('Japan', 2014)
+    initialDF = addTrip('Japan', 2014) ## NOTE: Yep, 2x same year
+    initialDF = addTrip('Israel', 2015)
+    initialDF = addTrip('Palestine', 2015)
+    initialDF = addTrip('Germany', 2016)
+    initialDF = addTrip('Senegal', 2016)
+    initialDF = addTrip('Argentina', 2016)
+    initialDF = addTrip('Japan', 2017)
+    initialDF = addTrip('United Kingdom', 2017)
+    initialDF = addTrip('Portugal', 2017)
+    initialDF = addTrip('Spain', 2017)
+    initialDF = addTrip('South Africa', 2017)
+    initialDF = addTrip('China', 2018)
+    initialDF = addTrip('Germany', 2018)
+    initialDF = addTrip('Egypt', 2018)
+    initialDF = addTrip('Jordan', 2018)
+    initialDF = addTrip('Italy', 2019)
+    initialDF = addTrip('Vatican City', 2019)
+    initialDF = addTrip('France', 2019)
+    initialDF = addTrip('Vietnam', 2019)
+    initialDF = addTrip('Kenya', 2020)
 
     ## Extend places lived by a few shades
     initialDF['Have Been'][initialDF['Code'] == 'FRA'] += 3 ## Just a summer
     initialDF['Have Been'][initialDF['Code'] == 'KOR'] += 5 ## One full year
     initialDF['Have Been'][initialDF['Code'] == 'ESP'] += 5 ## One full year
 
+    initialDF.to_csv('initiallyPopulated.csv', index=False)
     return initialDF
 
 
-def addTrip(df,country, yearWent):
-    ## Do a fuzzy-ish matcher
+def addTrip(country, yearWent):
+    df = pd.read_csv('initiallyPopulated.csv')
     dfCountryList = df['Country'].tolist()
+    df['Year Went'] = df['Year Went'].fillna('N/A')
     counter = 0
     countryOptions = list()
     for item in dfCountryList:
@@ -92,6 +90,7 @@ def addTrip(df,country, yearWent):
                 df.at[indexValue, 'Year Went'] = newlist
                 df['Have Been'][df['Country'] == item] = len(newlist)
     elif counter == 0:
+        ## Do a fuzzy-ish matcher
         print('\n WARNING: EMPLOYING FUZZY MATCH. CHECK THE FOLLOWING RESULT WAS AS INTENDED:')
         highest = process.extractOne(country,dfCountryList)
         print('Currently adding {}, the match to entry {} with a probability of {}.\n'.format(highest[0], country, highest[1]))
@@ -120,25 +119,30 @@ def addTrip(df,country, yearWent):
         print('Unfortunately more than one country can meet the name given. Please specify between the following: ', *countryOptions, sep='\n- ')
         exit(1)
 
+    df.to_csv('initiallyPopulated.csv', index=False) ## overwrites each time
     return df
 
 
 ##  Render everything
 def buildMap():
-    initiallyPopulatedDF = buildDF()
-    initiallyPopulatedDF.to_csv('initiallyPopulated.csv')
-    fig = px.choropleth(initiallyPopulatedDF, locations="Code", color='Have Been', hover_name='Country',hover_data=['Year Went'],color_continuous_scale=px.colors.sequential.Mint) #["green",'yellow','orange',"red"]) #px.colors.sequential.Plasma)
+    try:
+        df = pd.read_csv('initiallyPopulated.csv')
+    except: ## if it doesn't exist yet, then use seeded DF above
+        df = buildDF()
+    df['Year Went'] = df['Year Went'].fillna('N/A')
+    fig = px.choropleth(df, locations="Code", color='Have Been', hover_name='Country',hover_data=['Year Went'],color_continuous_scale=px.colors.sequential.Mint) #["green",'yellow','orange',"red"]) #px.colors.sequential.Plasma)
     fig.update_layout(
         title = "Places I've Been",
         coloraxis_showscale=False,
-        ## If I want to disable Automargins
+        # # If I want to disable Automargins
+        # autosize=False,
         # width=1500,
-        # height=1300,
+        # height=450,
         font=dict(
             family="Helvetica, monospace",
             size=18,
             color="#7f7f7f")
     )
-    fig.show()
+    # fig.show()
 
-buildMap()
+    return fig, df
---
+++ runApp.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import os, json, plotly, mapBuilder
+from flask import Flask, render_template, request, send_from_directory, redirect
+from flask_bootstrap import Bootstrap
+from datetime import datetime
+app = Flask(__name__)
+Bootstrap(app)
+
+## FLASK RENDER THE WEBPAGE:
+@app.route("/")
+def webFramesUnique():
+    returnedValues = mapBuilder.buildMap()
+    fig = returnedValues[0]
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template('index.html', graphJSON=graphJSON)
+
+## favicon
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
+
+## background process for docker image retag and push
+@app.route('/background_process', methods=['POST']) #, methods=['GET','POST'])
+def test():
+    if request.method == "POST":
+        data=request.form['data']
+        data2=request.form['data2']
+        if len(data2) == 0:
+            data2 = datetime.now().year
+        else:
+            data2 = int(data2)
+        # print(data, data2, type(data), type(data2))
+        mapBuilder.addTrip(data,data2)
+    return "Hello World" ## nothing, just to silence errors and will appear if you hit the route via browser
+
+
+### INITAITE IT VIA FLASK
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001) 
